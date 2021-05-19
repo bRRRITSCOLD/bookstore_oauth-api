ae14213069cd9566abd7db2666580d86cf86dbda
Blaine Richardson <blainerrichardson@gmail.com>
2021-05-18 20:31:19 -0700

    feat: section-6: finishing section-6 lecture 'Cassandra Go client'

--- repositories/database/access_token/access_token.go
+++ repositories/database/access_token/access_token.go
@@ -1,6 +1,7 @@
 package access_token_database_repository
 
 import (
+	cassandra_client "bookstore_oauth-api/clients/cassandra"
 	access_token_domain "bookstore_oauth-api/domains/access_token"
 	errors_utils "bookstore_oauth-api/utils/errors"
 )
@@ -17,5 +18,11 @@ func NewAccessTokenRepository() AccessTokenDatabaseRepository {
 }
 
 func (atDbRepo *accessTokenDatabaseRepository) GetByID(string) (*access_token_domain.AccessToken, *errors_utils.APIError) {
+	cassandraClientSession, err := cassandra_client.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer cassandraClientSession.Close()
+
 	return nil, errors_utils.NewInternalServerAPIError("database connection not implemented")
 }
